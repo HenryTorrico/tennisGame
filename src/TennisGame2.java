7a82f2276ac0fa7fb28b433598ff20a52a6fd6e5
HenryTorrico <guillerbauti@gmail.com>
2020-02-13 11:34:42 -0400

    obj1: encapsular las funciones del cuerpo del programa

--- src/TennisGame2.java
+++ src/TennisGame2.java
@@ -1,11 +1,11 @@
 
 public class TennisGame2 implements TennisGame
 {
-    public int P1point = 0;
-    public int P2point = 0;
+    public int P1point = 0; // nombres poco descriptivos
+    public int P2point = 0; // nombres poco descriptivos
     
-    public String P1res = "";
-    public String P2res = "";
+    public String P1res = ""; // nombres poco descriptivos
+    public String P2res = ""; // nombres poco descriptivos
     private String player1Name;
     private String player2Name;
 
@@ -14,59 +14,52 @@ public class TennisGame2 implements TennisGame
         this.player2Name = player2Name;
     }
 
-    public String getScore(){
+    public String getScore(){ // ifs anidados
         String score = "";
-        if (P1point == P2point && P1point < 4)
-        {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
-            score += "-All";
-        }
-        if (P1point==P2point && P1point>=3)
-            score = "Deuce";
+        score = tie(score);
+        score = deuce(score);
         
-        if (P1point > 0 && P2point==0)
+        score = normal(score);
+        score = normal1(score);
+        
+        score = normal2(score);
+        score = normal3(score);
+        
+        //muchas funciones hacen lo mismo
+        
+        score = advantage(score);
+        
+        score = win(score);
+        return score;
+    }
+
+	private String win(String score) {
+		if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2) // 2 es valor estatico
         {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
-            P2res = "Love";
-            score = P1res + "-" + P2res;
+            score = "Win for player1";
         }
-        if (P2point > 0 && P1point==0)
+        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2) // 2 es valor estatico
         {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
-            score = P1res + "-" + P2res;
+            score = "Win for player2";
+        }
+		return score;
+	}
+
+	private String advantage(String score) {
+		if (P1point > P2point && P2point >= 3) // 3 es valor estatico
+        {
+            score = "Advantage player1";
         }
         
-        if (P1point>P2point && P1point < 4)
+        if (P2point > P1point && P1point >= 3) // 3 es valor estatico
         {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
+            score = "Advantage player2";
         }
-        if (P2point>P1point && P2point < 4)
+		return score;
+	}
+
+	private String normal3(String score) {
+		if (P2point>P1point && P2point < 4) // 0-1-2-3 es valor estatico e ifs anidados
         {
             if (P2point==2)
                 P2res="Thirty";
@@ -78,27 +71,75 @@ public class TennisGame2 implements TennisGame
                 P1res="Thirty";
             score = P1res + "-" + P2res;
         }
-        
-        if (P1point > P2point && P2point >= 3)
+		return score;
+	}
+
+	private String normal2(String score) {
+		if (P1point>P2point && P1point < 4) // 0-1-2-3 es valor estatico e ifs anidados
         {
-            score = "Advantage player1";
+            if (P1point==2)
+                P1res="Thirty";
+            if (P1point==3)
+                P1res="Forty";
+            if (P2point==1)
+                P2res="Fifteen";
+            if (P2point==2)
+                P2res="Thirty";
+            score = P1res + "-" + P2res;
         }
-        
-        if (P2point > P1point && P1point >= 3)
+		return score;
+	}
+
+	private String normal1(String score) {
+		if (P2point > 0 && P1point==0)// 0-1-2-3 es valor estatico e ifs anidados
+        { 
+            if (P2point==1)
+                P2res = "Fifteen";
+            if (P2point==2)
+                P2res = "Thirty";
+            if (P2point==3)
+                P2res = "Forty";
+            
+            P1res = "Love";
+            score = P1res + "-" + P2res;
+        }
+		return score;
+	}
+
+	private String normal(String score) {
+		if (P1point > 0 && P2point==0) // 0-1-2-3 es valor estatico e ifs anidados
+        { 
+            if (P1point==1)
+                P1res = "Fifteen";
+            if (P1point==2)
+                P1res = "Thirty";
+            if (P1point==3)
+                P1res = "Forty";
+            P2res = "Love";
+            score = P1res + "-" + P2res;
+        }
+		return score;
+	}
+
+	private String tie(String score) {
+		if (P1point == P2point && P1point < 4)
         {
-            score = "Advantage player2";
+            if (P1point==0)
+                score = "Love";
+            if (P1point==1)
+                score = "Fifteen";
+            if (P1point==2)
+                score = "Thirty";
+            score += "-All";
         }
-        
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-        {
-            score = "Win for player1";
-        }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-        {
-            score = "Win for player2";
-        }
-        return score;
-    }
+		return score;
+	}
+
+	private String deuce(String score) {
+		if (P1point==P2point && P1point>=3) // el 3 es un valor estatico
+            score = "Deuce";
+		return score;
+	}
     
     public void SetP1Score(int number){
         
